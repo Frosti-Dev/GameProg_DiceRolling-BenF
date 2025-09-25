4d3e84f34970faf438b7a8927863317dad800c38
Frosti-Dev <bennyfranken3@gmail.com>
2025-09-25 10:25:31 -0300

    user input added...

---
+++ GameProg_DiceRolling-BenF/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProg_DiceRolling_BenF
+{
+    class Program
+    {
+        //variables
+        static int money = 100;
+
+        static int UserInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pick a number from 1 to 6...");
+                string MyUserInput = Console.ReadLine();
+                int InputasInt = int.Parse(MyUserInput);
+                
+                if (InputasInt  > 6)
+                {
+                    Console.WriteLine("Too High");
+                    continue;
+                }
+
+                else if (InputasInt < 0)
+                {
+                    Console.WriteLine("Too Low");
+                    continue;
+                }
+
+                else
+                {
+                    return InputasInt;
+                }
+            }
+        }
+
+        static void game(int UserInput)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            while (money > 0)
+            {
+                Console.WriteLine(UserInput());
+            }
+        }
+    }
+}
---
+++ GameProg_DiceRolling-BenF/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameProg_DiceRolling-BenF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameProg_DiceRolling-BenF")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b607eba-c43e-4cca-ab1f-7fd85025ff99")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
